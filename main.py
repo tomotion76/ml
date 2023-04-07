9a36ea87eefbf7405cb8dd19f1aa44f2e300a061
tomotion76 <130131810+tomotion76@users.noreply.github.com>
2023-04-07 16:11:39 +0900

    Add files via upload

--- main.py
+++ main.py
@@ -1,37 +1,76 @@
 import streamlit as st
-import openai
+import pandas as pd
+from pycaret.classification import *
 
-openai.api_key = "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"
+st.subheader('학습용 데이터 업로드')
 
-def generate_answer(prompt):
-    # 모델 엔진 선택
-    model_engine = "text-davinci-003"
+# 학습용 데이터 업로드
+# st.title('데이터 업로드')
+train_file = st.file_uploader('파일 업로드')
 
-    # 맥스 토큰
-    max_tokens = 2048
+# 학습용 파일 업로드
+if train_file:
+    # pd.read_csv()
+    train = pd.read_csv(train_file)# 코드 입력
 
-    # ChatGPT 요청 
-    completion = openai.Completion.create(
-        engine=model_engine,   
-        prompt=prompt,          # 프롬프트
-        max_tokens=max_tokens,  # 최대 단어수
-        temperature=0.3,        # creativity
-        top_p=1,
-        frequency_penalty=0,
-        presence_penalty=0
-    )
-    return completion
+# 예측 컬럼
+target = st.text_input('예측 컬럼명 입력')
 
+train_btn = st.button('학습 시작')
 
-st.title('chat GPT')
-
-st.file_uploader('파일 업로드')
-
-prompt = st.text_input('프롬프트')
-
-if prompt:
-    response = generate_answer(prompt)
-    # 생성된 글 출력
-    st.write(response.choices[0].text)
+if train_btn:
+    # setup
+    clf = setup(data=train, 
+                target=target, 
+                session_id=123, 
+                verbose=False, # False로 설정시 설정에 대한 출력생성을 생략합니다. (default=True)
+                ) # 코드 입력
     
-    #st.write(prompt)
+    # 학습 결과 출력
+    best_model = compare_models(sort='Accuracy', n_select=3, fold=5)# 코드 입력
+    # 모델 블렌딩
+    blended_models = blend_models(best_model, fold=5)# 코드 입력
+    # 학습 결과 받아오기
+    result = pull()
+    st.dataframe(result)
+    # 모델 저장
+    save_model(blended_models, 'classification')
+
+    
+# # CSV로 다운로드 
+@st.cache_data
+def convert_df(df):
+   return df.to_csv(index=False).encode('utf-8')
+
+
+# # 예측용 데이터 업로드
+st.subheader('예측용 데이터 업로드')
+test_file = st.file_uploader('예측용 파일 업로드')
+
+# # 예측용 파일 업로드
+if test_file:
+    # 코드 입력
+    test = pd.read_csv(test_file)
+    
+# # 예측 버튼
+test_btn = st.button('예측')
+
+# # 예측 버튼 클릭시
+if test_btn:
+    # 모델 로드 load_model()
+    loaded_model = load_model('classification') # 코드 입력
+    
+#     # 예측 predict_model()
+    predictions = predict_model(estimator=loaded_model, data=test)# 코드 입력
+    
+#     # 결과 다운로드
+    download_btn = st.download_button(
+        "예측 결과 다운로드",
+        convert_df(predictions),
+        "prediction.csv",
+        "text/csv",
+        key='download-csv'
+    )
+
+
+    
